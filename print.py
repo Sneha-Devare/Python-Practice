d7da91ca242576ccb7b61153d7c92915e1da8f55
Sneha-Devare <Snehadevare158@gmail.com>
2024-12-22 15:45:08 +0530

    new commit

---
+++ print.py
@@ -0,0 +1,31 @@
+#write a program to display "hello,[your name]"
+name="sneha"
+print("hello",name)
+
+#print your name, age and favourite hobby
+name="sneha"
+age="20"
+hobby="analyzing"
+#use f for string formatting
+print(f"my name is {name}, I am {age} years old and my hobby is {hobby}.")
+
+a=20
+b=30
+print(a+b)
+
+#multi-line output
+print("python is fun\nNow I am learning it")
+
+#already gives spaces
+print("python","is","fun") 
+
+print("Sneha","Rajendra","Devare",sep="-")
+
+#By default, print() ends with a newline. You can change it using the end parameter
+print("Hello",end=" ")
+print("world")
+
+#Use r before a string to treat backslashes as normal characters
+print(r"C:\Users\Sneha\Documents")
+
+
