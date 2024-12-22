5a62f72be2d96814a724ac5685eca4e30e0f1538
Sneha-Devare <Snehadevare158@gmail.com>
2024-12-23 00:59:06 +0530

    commit

---
+++ loops.py
@@ -0,0 +1,6 @@
+#for loop
+#Used to iterate over a sequence (like a list, string, or range) and execute a block of code for each item in that sequence.
+
+#print numbers 1 to 5
+for i in range(6):
+    print(i)
---
+++ range.py
@@ -0,0 +1,11 @@
+#range function in python
+#The range() function is used to generate a sequence of numbers. It's commonly used in loops like for loops. You can specify the start, stop, and step values.
+
+r = range(5)
+print(r)  # Output: range(0, 5)
+
+r = range(5)  # 0 to 4
+print(list(r))
+
+for i in range(5):
+    print(i)
