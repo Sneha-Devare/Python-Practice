4d4581434f9b36efe480bb5f3966cfea35b4f184
Sneha-Devare <Snehadevare158@gmail.com>
2024-12-23 19:14:56 +0530

    new commit

--- loops.py
+++ loops.py
@@ -1,6 +1,19 @@
 #for loop
 #Used to iterate over a sequence (like a list, string, or range) and execute a block of code for each item in that sequence.
 
-#print numbers 1 to 5
+#print numbers 0 to 5
 for i in range(6):
     print(i)
+
+
+#While Loop: Repeats as long as a condition is True.
+count = 0
+while count < 5:  # Loops until count is less than 5
+    print(count)
+    count += 1
+
+
+cnt = 0
+while cnt < 10:
+    print(cnt)
+    cnt +=1
